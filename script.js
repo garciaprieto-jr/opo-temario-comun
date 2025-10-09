32830dc3d1b783275e98daabc2e79e56cad70f46
garciaprieto-jr <garciaprieto.jr@gmail.com>
2025-10-09 16:06:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,13 @@
 import { tema1 } from './tema_1.js';
 import { tema2 } from './tema_2.js';
 import { tema3 } from './tema_3.js';
-import { examen2024 } from './examen_2024_01.js';
-import { comun_otros_años } from './comun_otros_años.js';
 
 // Consolidación de temas: Mapea los data-tema del HTML a los objetos importados.
 const temas = {
     'tema1': tema1,
     'tema2': tema2,
     'tema3': tema3,
-    'examen2024': examen2024,
-    'comun_otros_años': comun_otros_años,
+   
 };
 
 // Constante para la clave de localStorage
